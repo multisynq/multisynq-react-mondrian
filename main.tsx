94ea4af21214bc56eefc0bf2c62066abb755d497
afonsocrg <afonso.crg@gmail.com>
2024-03-24 17:02:54 +0000

    do not use autoSession for appId

--- main.tsx
+++ main.tsx
@@ -1,6 +1,6 @@
 import { createRoot } from 'react-dom/client'
 
-import { CroquetRoot, App as CroquetApp } from '@croquet/react'
+import { CroquetRoot } from '@croquet/react'
 import PaintingModel from './src/models/painting'
 
 import App from './src/App'
@@ -11,7 +11,7 @@ createRoot(container!).render(
     sessionParams={{
       name: 'painting',
       model: PaintingModel,
-      appId: import.meta.env['VITE_CROQUET_APP_ID'] || CroquetApp.autoSession('q'),
+      appId: import.meta.env['VITE_CROQUET_APP_ID'],
       apiKey: import.meta.env['VITE_CROQUET_API_KEY'],
       password: 'abc',
     }}
