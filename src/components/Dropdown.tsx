27cf34551f19260b0fc8af4e3eb928cf49e8b240
afonsocrg <afonso.crg@gmail.com>
2024-04-05 15:40:48 +0100

    improve select styles

--- src/components/Dropdown.tsx
+++ src/components/Dropdown.tsx
@@ -9,17 +9,16 @@ type DropdownProps<T> = {
 }
 export default function Dropdown<T>({ selected, options, onChange }: DropdownProps<T>) {
   return (
-    <select {...{
-      value: selected,
-      onChange: (e) => onChange(e.target.selectedIndex),
-      style: {
-        border: '0.1rem solid black',
-        borderRadius: '0.5rem',
-        padding: '0.2rem',
-      }
-    }}>
+    <select
+      {...{
+        value: selected,
+        onChange: (e) => onChange(e.target.selectedIndex),
+      }}
+    >
       {options.map((option, i) => (
-        <option key={i} value={i}>{option.label}</option>
+        <option key={i} value={i}>
+          {option.label}
+        </option>
       ))}
     </select>
   )
